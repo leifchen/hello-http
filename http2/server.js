6cdf1b3af54463c0c4cf1c041013f31f8ae07e8c
LeifChen <leifchen90@gmail.com>
2018-08-11 16:50:02 +0800

    feat: add http2

---
+++ http2/server.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer((request, response) => {
+    console.log('request come', request.url)
+
+    const html = fs.readFileSync('test.html', 'utf-8')
+    const img = fs.readFileSync('test.jpg')
+    if (request.url === '/') {
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Connection': 'close',
+            'Link': '</test.jpg>; as=image; rel=preload'
+        })
+        response.end(html)
+    } else {
+        response.writeHead(200, {
+            'Content-Type': 'image/jpg',
+            'Connection': 'close'
+        })
+        response.end(img)
+    }
+}).listen(3000)
+
+console.log('server listening on port:3000')
